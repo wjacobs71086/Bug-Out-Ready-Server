9f41f8d97aa0fbfa655ebac865f0b0fe8aa6aefa
Wesley Jacobs <w.jacobs.632@gmail.com>
2019-11-22 11:16:38 -0800

    all server funcionality is ready, next is testing

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,25 @@
+process.env.TZ = 'UCT';
+process.env.JWT_SECRET = '1Forrest1';
+
 const { expect } = require( 'chai' );
 const supertest = require( 'supertest' );
 
 global.expect = expect;
 global.supertest = supertest;
+
+
+
+// 
+// process.env.NODE_ENV = 'test';
+// 
+
+// require('dotenv').config();
+
+// process.env.TEST_DB_URL = process.env.TEST_DB_URL
+//   || "postgresql://dunder_mifflin:password-jim@localhost/blogful-auth-test";
+
+// const { expect } = require('chai');
+// const supertest = require('supertest');
+
+// global.expect = expect;
+// global.supertest = supertest;

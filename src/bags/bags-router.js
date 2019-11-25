f90f56dd603256fa223baf0033b9be955587da01
Wesley Jacobs <w.jacobs.632@gmail.com>
2019-11-25 15:20:35 -0800

    triage delete

--- src/bags/bags-router.js
+++ src/bags/bags-router.js
@@ -35,7 +35,7 @@ bagsRouter
             error: 'Delete unsuccessful'
           });
         } else {
-          return res.status(204);
+          return res.status(204).end();
         }
       });
   })
